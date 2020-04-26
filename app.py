4f704f59d5f1f598653742b75db30ebb83681616
Alyssa <allym.moyer21@gmail.com>
2020-04-26 15:45:23 -0700

    first flask route done

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #references to tables
-station = Base.classes.station
-measurment = Base.classes.measurment
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 #Flask App
 app = Flask(__name__)
@@ -24,3 +24,37 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#home page that lists all routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query measurement table for all dates and precipitation values
+    results = session.query(Measurement.date, Measurement.prcp ).all()
+
+    session.close()
+
+    # Create a dictionary from the queried data. 
+    # Loop through results and add dictionary value with date as key and prcp as value.
+    precip_dates = []
+    for date, prcp in results:
+        date_dict = {}
+        date_dict[date] = prcp
+        precip_dates.append(date_dict)
+
+#return the jsonified dictionary
+    return jsonify(precip_dates)
+
